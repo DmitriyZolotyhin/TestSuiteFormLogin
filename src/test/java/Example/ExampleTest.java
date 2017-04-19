93f2624cf796a755a59e7177f8e968216a713f3d
Dmitriy <zolotoy18.12.1988@gmail.com>
2017-04-19 17:06:32 +0400

    Removed priority, fixed the method logInWithInputInvalidEmal

--- src/test/java/Example/ExampleTest.java
+++ src/test/java/Example/ExampleTest.java
@@ -9,98 +9,98 @@ import org.testng.annotations.Test;
  */
 public class ExampleTest {
 
-    @Test(priority = 1,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void logInLogOut()
     {
         LogInLogOut.logInLogOut1();
 
     }
 
-    @Test(priority = 10,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithVoidValue()
     {
         LogInWithVoidValue.logInWithVoidValue1();
     }
 
 
-    @Test(priority = 9,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithoutInputEmail()
     {
         LogInWithOutInputEmail.logInWithoutInputEmail1();
     }
 
 
-    @Test(priority = 7,groups = {"negative_test"})   //Нажата радио кнопка Email
+    @Test(groups = {"negative_test"})    //Нажата радио кнопка Email
     public void logInWithoutInputPassword()
     {
         LogInWithOutInputPassword.logInWithoutInputPassword1();
     }
 
 
-    @Test(priority = 8,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithoutInputPhone()
     {
         LoginInWithOutInputPhone.logInWithoutInputPhone1();
     }
 
 
-    @Test(priority = 13 ,groups = {"negative_test" })//Нажата радио кнопка Телефон
+    @Test(groups = {"negative_test" })  //Нажата радио кнопка Телефон
     public void logInWithoutInputPasswordPressedRadioButtonPhone()
     {
         LogInWihtOutInputPasswordPreseedRadioButtonPhone.logInWithoutInputPasswordPressedRadioButtonPhone1();
     }
 
 
-    @Test(priority = 10,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithVoidValuePressedRadioButtonPhone()
     {
         LogInWithVoidValuePressedRadioButtonPhone.logInWithVoidValuePressedRadioButtonPhone1();
     }
 
 
-    @Test(priority = 2 ,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithInputInvalidEmal()
     {
         LoginInWithInputInvalidEmail.logInWithInputInvalidEmal1();
     }
 
 
-    @Test(priority = 4,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithInputInvalidPassword()
     {
         LogInWithInputInvalidPassword.logInWithInputInvalidPassword1();
     }
 
 
-    @Test(priority = 5,groups = {"negative_test"})  //Нажата радио кнопка Телефон
+    @Test(groups = {"negative_test"})  //Нажата радио кнопка Телефон
     public void logInWithInputInvalidPhonePressed()
     {
         LogInWithInputInvalidPhonePressed.logInWithInputInvalidPhonePressed1();
     }
 
 
-    @Test(priority = 11,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void logInWithShowPassword()
     {
         LogInWithShowPassword.logInWithShowPassword1();
     }
 
 
-    @Test(priority = 12,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkImageDns1()
     {
         FormLogInWithClickLinkImageDns1.formlogInWithClickLinkImageDns1();
     }
 
 
-    @Test(priority = 6,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkImageDns2()
     {
         FormLogInWithClickLinkImageDns2.formlogInWithClickLinkImageDns2_2();
     }
 
 
-    @Test(priority = 14,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkImageDns3()
 
     {
@@ -109,7 +109,7 @@ public class ExampleTest {
 
 
 
-    @Test(priority = 15,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkImageDns4()
 
     {
@@ -117,7 +117,7 @@ public class ExampleTest {
     }
 
 
-    @Test(priority = 16,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkForgotPassword() // проверка нажататия на "Забыли пароль?"
 
     {
@@ -125,7 +125,7 @@ public class ExampleTest {
     }
 
 
-    @Test(priority = 17,groups = {"positive_test"})
+    @Test(groups = {"positive_test"})
     public void formlogInWithClickLinkRegistration() // проверка нажататия на "Регистрация"
 
     {
@@ -134,7 +134,7 @@ public class ExampleTest {
     }
 
 
-    @Test(priority = 18,groups = {"negative_test"})
+    @Test(groups = {"negative_test"})
     public void LogInWithInputInvalidPasswordPressedRadioButtonPhone()
 
     {
--- src/test/java/Example/LoginInWithInputInvalidEmail.java
+++ src/test/java/Example/LoginInWithInputInvalidEmail.java
@@ -9,54 +9,65 @@ import org.testng.Assert;
 
 import java.util.concurrent.TimeUnit;
 
+
+
 /**
  * Created by Администратор on 17.04.2017.
  */
 public class LoginInWithInputInvalidEmail {
-    public static void logInWithInputInvalidEmal1()
 
+
+
+    public static void logInWithInputInvalidEmal1()
     {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("http://www.dns-shop.ru/");
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
         //Нажать Войти
         WebElement searchButton = driver.findElement(By.cssSelector("a#loginButton.dropdown-toggle"));
         searchButton.click();
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        //driver.navigate().refresh();// обновить браузер
+
 
         // Поиск поля ввода логин и ввод некорректного значения
+        // Найти поле логин и ввести некоректный телефон
         WebElement searchField = driver.findElement(By.cssSelector("input#login.first.placeholder-right"));
         searchField.clear();
-        searchField.sendKeys("ru");
+        searchField.sendKeys("00");
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+        // Найти поле пароль и очистить
+        WebElement searchField2 = driver.findElement(By.cssSelector("input#password.second"));
+        searchField2.clear();
+
+        //Поиск ошибки
+        WebElement searchFieldError = driver.findElement(By.cssSelector("div.error"));
+        String strng2 = searchFieldError.getText();
+        Assert.assertEquals("Некорректный e-mail", strng2);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        //Поиск и очистка поля ввода пароль
-        WebElement searchField2 = driver.findElement(By.cssSelector("input#password.second"));
-        searchField2 .click();
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        //Найти поле пароль и очистить
+        WebElement searchField3 = driver.findElement(By.cssSelector("input#password.second"));
+        searchField3.clear();
 
-        //Поиск сообщения об ошибке
-       WebElement searchFieldError = driver.findElement(By.cssSelector("div.error"));
-     String strng = searchFieldError.getText();
-        Assert.assertEquals("Некорректный e-mail", strng);
-       // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-
-        // Нажатие кнопки Войти
-      //  WebElement searchButtonEnter = driver.findElement(By.cssSelector("input#login-btn.btn.active-btn"));
-      //  JavascriptExecutor executor = (JavascriptExecutor) driver;
-      //  executor.executeScript("arguments[0].click();",searchButtonEnter );
-       // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //Нажать кнопку Войти
+       // WebElement searchButtonEnter2 = driver.findElement(By.cssSelector("input#login-btn.btn.active-btn"));
+     //   searchButtonEnter2.click();
         WebElement searchButtonEnter = driver.findElement(By.cssSelector("input#login-btn.btn.active-btn"));
-        searchButtonEnter.click();
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+         JavascriptExecutor executor = (JavascriptExecutor) driver;
+          executor.executeScript("arguments[0].click();",searchButtonEnter );
+         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 
         //Поиск сообщения об ошибке
         WebElement searchFieldError2 = driver.findElement(By.cssSelector("div.error"));
-        String strng2 = searchFieldError2.getText();
-        Assert.assertEquals("Не все поля заполнены", strng2);
+        String strng3 = searchFieldError2.getText();
+        Assert.assertEquals("Не все поля заполнены", strng3);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         driver.quit();
     }
 }
+
